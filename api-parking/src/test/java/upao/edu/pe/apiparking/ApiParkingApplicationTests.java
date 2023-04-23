2f86e7be60676041e9138f386edf0a60e1c0c12d
Mbgg08 <mgutierrezg11@upao.edu.pe>
2023-04-22 21:06:33 -0500

    Estructura de proyecto api parking - inicial

---
+++ api-parking/src/test/java/upao/edu/pe/apiparking/ApiParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package upao.edu.pe.apiparking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiParkingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
